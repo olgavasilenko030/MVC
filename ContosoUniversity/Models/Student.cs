d1cd84d50f3c268ed34b8685a64e4a31b3ceffda
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-09-27 23:34:46 +0500

    Add project files.

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; } // Дата поступления
+        /// Navigation properties
+        public ICollection <Enrollment> Enrollments { get; set; }
+    }
+}
